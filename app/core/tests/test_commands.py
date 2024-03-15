817887c234bafc92ac817ebbb500c8816a33b0f0
Patrick Schwan <info@pschwan.de>
2024-03-15 12:14:47 +0100

    corrected linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Tests commands."""
@@ -32,7 +33,7 @@ class CommandTests(SimpleTestCase):
         # 3 Errors while preparing db
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
